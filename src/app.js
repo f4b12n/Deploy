9ccd6c839e7dc65f66123b62ef0f67264dc9af0a
Fabian Villa <Fabianmiguel1998@gmail.com>
2024-05-23 13:45:18 -0400

    Se hicieron cambios y modificaciones para que funcionara mejor

--- src/app.js
+++ src/app.js
@@ -7,69 +7,107 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoUri = process.env.MONGODB_URI
+const mongoUri = process.env.MONGODB_URI;
 
 try {
-    mongoose.connect(mongoUri);
-    console.log("Conectado a MongoDB");
+  mongoose.connect(mongoUri);
+  console.log("Conectado a MongoDB");
+
 } catch (error) {
-    console.log("Error de conexión", error);
+  console.error("Error de conexión", error);
 }
 
 const libroSchema = new mongoose.Schema({
-    titulo: String,
-    autor: String,
+  titulo: String,
+  autor: String,
 });
 
 const Libro = mongoose.model("Libro", libroSchema);
 
-const libros = [
-    {id: 1, titulo: "1984", autor: "George Orwel"},
-    {id: 2, titulo: "Cien años de soledad", autor: "Gabriel Garcia Marquez"},
-    {id: 3, titulo: "Dune", autor: "Frank Herbert"}
-]
-
+//Ruta raiz
 app.get("/", (req, res) => {
-    res.send("Bienvenidos a la tienda de libros");
-})
+  res.send("Bienvenido a la tienda de libros");
 
-app.get("/libros", (req, res) => {
-    res.json(libros);
-})
+});
 
-app.get("/libros/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const libro = libros.find((libro) => libro.id === id)
-    if (libro) {
-        res.json(libro);
-    }else {
-        res.status(404).send("Libro no encontrado");
-    }
-})
-
-//Crear un nuevo libro
+// Crear nuevo libro
 app.post("/libros", async (req, res) => {
-    const libro = new Libro({
-        titulo: req.body.titulo,
-        autor: req.body.autor,
-    })
+  const libro = new Libro({
+    titulo: req.body.titulo,
+    autor: req.body.autor,
+  });
 
-    try {
-        await libro.save();
-        res.json(libro);
-    } catch (error) {
-        res.status(500).send("Error al guardar libro");
-    }
+  try {
+    await libro.save();
+    res.json(libro);
+
+  } catch (error) {
+    res.status(500).send("Error al guardar el libro");
+  }
+
 });
 
 //Traer un listado de todos los libros
-app.get ("/libros", async (req, res) => {
-    try {
-        const libro = await Libro.find();
-        res.json(libros);
-    } catch (error) {
-        res.status(500).send("Error al obtene los libros", error);
-    }
+app.get("/libros", async (req, res) => {
+  try {
+    const libros = await Libro.find();
+    res.json(libros);
+
+  } catch (error) {
+    res.status(500).send("Error al obtener los libros", error);
+
+  }
 });
+
+//Ruta para encontrar libro por su ID
+app.get("/libros/:id", async (req, res) => {
+  try {
+    const libro = await Libro.findById(req.params.id);
+    if (libro) {
+      res.json(libro);
+    } else {
+      res.status(404).send("Libro no encontrado");
+    }
+  } catch (error) {
+    res.status(500).send("Error al buscar el libro");
+  }
+});
+
+//Ruta para encontrar y actualizar por ID
+app.put("/libros/:id", async (req, res) => {
+  try {
+    const libro = await Libro.findByIdAndUpdate(
+      req.params.id,
+      {
+        titulo: req.body.titulo,
+        autor: req.body.autor,
+      },
+      { new: true } // Esta opción hará que se devuelva el documento actualizado
+    );
+
+    if (libro) {
+      res.json(libro);
+    } else {
+      res.status(404).send("Libro no encontrado");
+    }
+  } catch (error) {
+    res.status(500).send("Error al actualizar el libro");
+  }
+});
+
+//Ruta para eliminar por ID
+app.delete("/libros/:id", async (req, res) => {
+  try {
+    const libro = await Libro.findByIdAndDelete(req.params.id);
+    if (libro) {
+      res.status(204).send("El libro fué eliminado");
+    } else {
+      res.status(404).send("Libro no encontrado");
+    }
+  } catch (error) {
+    res.status(500).send("Error al eliminar el libro");
+  }
+});
+
 
 module.exports = app;
